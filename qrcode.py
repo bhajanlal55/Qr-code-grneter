ca8d292ce965863d90bd71a498c6cff02a478202
BHAJANLAL <bhajanlalpanwarjio@gmail.com>
2025-09-03 10:06:28 +0530

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -1,1 +1,13 @@
 
+import qrcode
+
+# User se input lena
+data = input("👉 QR Code me kya dalna chahte ho? URL/Text likho: ")
+
+# QR Code generate karna
+img = qrcode.make(data)
+
+# File save karna
+img.save("/sdcard/myqrcode.png")
+
+print("✅ QR Code ban gaya: /sdcard/myqrcode.png")
